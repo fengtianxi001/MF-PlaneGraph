e56b6fc71b1f7b77f597fe490b445eeb33b4ba3d
fengtianxi001 <625362207@qq.com>
2025-02-08 14:49:40 +0800

    feat: 修改leaflet默认样式

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'leaflet/dist/leaflet.css'
-import ArcoVue from '@arco-design/web-vue'
+import Arco from '@arco-design/web-vue'
 import '@arco-design/web-vue/dist/arco.less'
+import '@/assets/styles/reset.scss'
+import 'leaflet/dist/leaflet.css'
 
 const boostrap = async () => {
   const app = createApp(App)
-  app.use(ArcoVue)
+  app.use(Arco)
   app.mount('#app')
 }
 
